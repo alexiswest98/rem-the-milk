87844589159fb4b426944dca0c26ad6cd9fc1032
evan <ebmorgansb@gmail.com>
2022-12-16 00:00:41 -0800

    wow

--- react-app/src/store/specTasks.js
+++ react-app/src/store/specTasks.js
@@ -28,6 +28,7 @@ export const getAllTasksByDayThunk = () => async (dispatch) => {
     const response = await fetch('/api/tasks/day');
     if (response.ok) {
         const tasks = await response.json();
+        console.log('in the thunk for dayTasks', tasks)
         dispatch(getAllTasksDayAction(tasks));
     };
 };
