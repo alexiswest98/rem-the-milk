b68952840e3eb107484287bf8e783d258de2abb4
Gabriel <gabetd0904@gmail.com>
2022-12-17 14:24:29 -0600

    fixed

--- react-app/src/components/ListTasks/compTaskHome.js
+++ react-app/src/components/ListTasks/compTaskHome.js
@@ -85,7 +85,7 @@ export default function CompTaskPage() {
                                 <h4>Group Lists</h4>
                                 {groupLists.map(list => (
                                     <Link to={`/lists/${list.id}`}>
-                                    <h4>{list.name}</h4>
+                                    <h4>{list.name} <EditListModal listId={list.id}/></h4>
                                     </Link>
                                 ))}
                             </div>
