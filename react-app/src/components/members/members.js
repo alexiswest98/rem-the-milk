d804b65560decd329b06c5f1c71775418b27280a
Gabriel <gabetd0904@gmail.com>
2022-12-19 09:41:53 -0600

    final

--- react-app/src/components/members/members.js
+++ react-app/src/components/members/members.js
@@ -20,7 +20,7 @@ export default function Members({setShowModal}) {
     })
     // console.log('members id"s = ', membersId)
   const viable = followers.filter(follow => {
-    return !membersId.includes(follow.id)
+    return !membersId.includes(follow.id) && follow.id
   })
   console.log(viable)
     const add = async(id) => {
