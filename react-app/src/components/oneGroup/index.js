feefb6bb065d2970935c065ecb487996f80ffd8e
michaellacey <mikelacey123123@gmail.com>
2022-12-19 01:55:20 -0500

    one group page

--- react-app/src/components/oneGroup/index.js
+++ react-app/src/components/oneGroup/index.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import './index.css'
-import { Link, useParams} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { getAllGroupsThunk } from "../../store/groups";
 import { deleteGroupThunk } from "../../store/groups";
 import { GetAllListsThunk } from "../../store/lists";
@@ -13,75 +13,91 @@ import AddMemberModal from "../members";
 import CreateGroupListModal from "../Groups";
 // import Members from "../members/members";
 export default function GetOneGroup() {
-    // const history = useHistory()
-    const dispatch = useDispatch();
-    const { groupId } = useParams();
-    // Listen for change of state and grab groups from the slice
-    const group = useSelector(state => state.groups[groupId]);
-    // const currUser = useSelector(state => state.session)
-    const lists = useSelector(state => state.lists)
-    const members = Object.values(useSelector(state => state.members))
-    const groupLists = Object.values(lists).filter(list => {
-       return list.group_id == groupId})
+  // const history = useHistory()
+  const dispatch = useDispatch();
+  const { groupId } = useParams();
+  // Listen for change of state and grab groups from the slice
+  const group = useSelector(state => state.groups[groupId]);
+  // const currUser = useSelector(state => state.session)
+  const lists = useSelector(state => state.lists)
+  const members = Object.values(useSelector(state => state.members))
+  const groupLists = Object.values(lists).filter(list => {
+    return list.group_id == groupId
+  })
 
-        // console.log('grouplists=',groupLists)
-        // console.log('group=',group)
+  // console.log('grouplists=',groupLists)
+  // console.log('group=',group)
 
-    const remove = async(id) => {
-        dispatch(RemoveMemberThunk(groupId, id))
-      }
-    //   const owner = group.owner_id === currUser.id
+  const remove = async (id) => {
+    dispatch(RemoveMemberThunk(groupId, id))
+  }
+  //   const owner = group.owner_id === currUser.id
 
 
-    useEffect(() => {
-        dispatch(getAllGroupsThunk());
-        dispatch(GetAllListsThunk(groupId));
-        dispatch(GetMembersThunk(groupId))
-    }, [groupId, dispatch]);
+  useEffect(() => {
+    dispatch(getAllGroupsThunk());
+    dispatch(GetAllListsThunk(groupId));
+    dispatch(GetMembersThunk(groupId))
+  }, [groupId, dispatch]);
 
-    const deleteList = (list_id) => {
-        dispatch(DeleteListThunk(list_id))
-      }
-    if (!group) return null
+  const deleteList = (list_id) => {
+    dispatch(DeleteListThunk(list_id))
+  }
+  if (!group) return null
 
-    return (
-        <div className="groupsDiv">
-            <div>
-                    <div className="eachGroupMap">
-                        <img className='oneGroupImg' src={`${group.image_url}`} alt='group Pic'></img>
-                        <div className="oneGroup">{group.name}
-                        <AddMemberModal/>
-                        </div>
-                    </div>
-                    {Object.values(groupLists).map(list=> (
-                        <div>
-                            <p>_________________________________</p>
-                            <p>{list.name}       due: {list.due}</p>
-                            <p>{list.notes}</p>
-                            <button onClick={()=> deleteList(list.id)}>delete</button>
-
-                        </div>
-                    ))}
-                    <CreateGroupListModal groupId={groupId}/>
-                    <Link to='/dashboard'>
-                    <button onClick={()=> {dispatch(deleteGroupThunk(groupId))}}>Delete your group</button>
-                    </Link>
-
-        </div>
-        <div className="membersTotal">
-        <h1 className="MemberTitle">Group Members</h1>
-        <div className="membersTotal">
-        {members.map(member=>(
-          <div>
-          <div className="member-list">
-            <button onClick={() => remove(member.id)}>remove from group</button>
-            {member.username}
-            </div>
-
+  return (
+    <div className="groupsDiv">
+      <div className="oneGroupTopHalf">
+        <div className="eachGroupMap">
+          <img className='oneGroupImg' src={`${group.image_url}`} alt='group Pic'></img>
+          <div className="oneGroup">{group.name}
+            <AddMemberModal />
           </div>
-        ))}
         </div>
+        <h4 className="onegroupListHeader">Lists for the group</h4>
+        <div className="listMappedBigDiv">
+          {Object.values(groupLists).map(list => (
+            <div className="notesMapped">
+              <Link className="groupListLink" key={`a{${list.id}`} to={`/lists/${list.id}`}>
+
+                <p id="listDueName">{list.name}       </p>
+                <p id="listDueName">due: {list.due}</p>
+
+              </Link>
+              <p id="listDueName">{list.notes}</p>
+              <button onClick={() => deleteList(list.id)} className='deleteListInGroup'>delete</button>
+
+            </div>
+          ))}
         </div>
+        <div className="createListDeleteGroup">
+          <CreateGroupListModal groupId={groupId} />
+          <Link to='/dashboard'>
+            <button onClick={() => { dispatch(deleteGroupThunk(groupId)) }} className='deleteGroupBtn'>Delete your group</button>
+          </Link>
+        </div>
+
+
+      </div>
+      <div className="membersTotal">
+        <h1 className="MemberTitle">Group Members</h1>
+        <div className="membersTotal2">
+          {members.map(member => (
+            <div className="outerMembers-list">
+              <div className="member-list">
+                <div className="memberDivs">
+                  {member.username}
+                </div>
+                <div className="memberDivs">
+                  {member.email}
+                </div>
+                <button onClick={() => remove(member.id)} className='RemoveFromGroupBtn'>remove from group</button>
+              </div>
+
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-    )
+  )
 }
