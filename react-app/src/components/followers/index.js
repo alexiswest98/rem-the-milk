c3894a970759073ed6a52f27bc9091c6f05bdbbe
Alexis West <alexishwest98@gmail.com>
2022-12-18 19:15:55 -0800

    ignore

--- react-app/src/components/followers/index.js
+++ react-app/src/components/followers/index.js
@@ -2,32 +2,33 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './index.css'
 // import { Link } from 'react-router-dom'
-import { getFollowsThunk } from "../../store/follows";
-import { getAllUsersThunk } from "../../store/users";
+import { getFollowingThunk } from "../../store/follows";
+// import { getAllUsersThunk } from "../../store/users";
 
 
 export default function GetFollowers() {
     const dispatch = useDispatch();
-
-    const followers = Object.values(useSelector(state => state.follows));
+    let followers = Object.values(useSelector(state => state.follows));
     // console.log('followers ----', followers);
 
+    if (!followers) return null;
 
     useEffect(() => {
-        dispatch(getFollowsThunk())
-        dispatch(getAllUsersThunk())
+        dispatch(getFollowingThunk())
     }, [dispatch])
 
     return (
         <div className="followers-component">
             <div className="follow-div">
-            <h1 className="followers-title">FOLLOWERS</h1>
+                <h1 className="followers-title">FOLLOWERS</h1>
             </div>
             <div className="follower-details">
                 {Object.values(followers).map(follower => (
                     <div className="prof-follower-div">
                         <div className="outer-prof-div-shell">
-                            <img className="followerImg" src={follower.image_url} />
+                            <div className="outer-name-img-shell">
+                                <img className="followerImg" src={follower.image_url} />
+                            </div>
                             <div className="outer-name-div-shell">
                                 <h4>{follower.username}</h4>
                             </div>
